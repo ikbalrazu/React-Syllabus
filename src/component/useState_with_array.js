2956ad5388a7fa645ae61b3bb302cfa926f78df9
Md Ikbal Razu <37942730+ikbalrazu@users.noreply.github.com>
2021-09-22 14:09:35 +0600

    added some array value

--- src/component/useState_with_array.js
+++ src/component/useState_with_array.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {useState} from 'react';
 
 function UseState_with_array(){
-    const [list,setList]=useState([1,6,3,4,5])
+    const [list,setList]=useState([1,6,3,4,5,7,8])
     /*
     const generatelist=()=>{
         setList([...list, {
